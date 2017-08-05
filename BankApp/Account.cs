ad73486dfc3becf14452afb556e2efdc045b4972
Leilani Raranga (Inconsulting Inc.) <v-lerara@microsoft.com>
2017-08-04 23:24:50 -0700

    Add project files.

---
+++ BankApp/Account.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetCalculator
+{
+    /// <summary>
+    /// This class describes the user's account
+    /// </summary>
+    class Account
+    {
+        #region static
+        private static int lastAccountNumber = 0;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Email address of the user
+        /// </summary>
+        public string EmailAddress { get; set; }
+        public int AccountNumber { get; private set; }
+        public DateTime CreatedDate { get; private set; }
+        public double Salary { get; set; }
+        //public double MonthlySalary { get;  set; }
+        //public decimal Savings { get; private set; }
+        //public decimal Needs { get; private set; }
+        //public decimal Wants { get; private set; }
+
+        #endregion
+        #region constructors
+        public Account()
+        {
+            AccountNumber = ++lastAccountNumber;
+            CreatedDate = DateTime.Now;
+        }
+        // overloading method - same name diff parameters
+        // use 'this' for constructors only not methods
+        //public Account(string emailAddress) : this()
+        //{
+        //    EmailAddress = emailAddress;
+        //}
+        //public Account(string emailAddress, double salary): this(emailAddress)
+        //{
+        //    Salary = salary;
+        //}
+
+
+        #endregion
+
+    }
+}
---
+++ BankApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAccount = new Account
+            {
+                EmailAddress = "user1@mail.com",
+                Salary = 70000
+        };
+
+            Console.WriteLine(
+                        $"Account Number: {myAccount.AccountNumber}," +
+                        $"Email Address: { myAccount.EmailAddress}," +
+                        $"Account Creation Date: { myAccount.CreatedDate}," +
+                        $"Salary: { myAccount.Salary:c}");
+            Console.WriteLine(
+                "According to your salary, your budget should be : " +
+                "Savings: {0}, " +
+                "Wants: {1}, " +
+                "Needs: {2}", 
+                (myAccount.Salary /12) *0.3, 
+                (myAccount.Salary/12) * 0.5, 
+                (myAccount.Salary/12 )* 0.2
+                );
+
+            //var myAccount2 = new Account();
+            //myAccount2.EmailAddress = "user2@mail.com";
+            //myAccount2.Salary = 25000;
+
+            //Console.WriteLine(
+            //            $"Account Number: {myAccount2.AccountNumber}," +
+            //            $"Email Address: { myAccount2.EmailAddress}," +
+            //            $"Account Creation Date: { myAccount2.CreatedDate}," +
+            //            $"Salary: { myAccount2.Salary:c}");
+
+             Console.ReadKey();
+        }
+    }
+}
